9cd645cfddc889168bc5e1e03e9d68cfa4efaa5f
RadekM86 <byrm.design@gmail.com>
2018-02-23 22:34:21 +0100

    Rebuilding algorothm

--- public/algorithm.js
+++ public/algorithm.js
@@ -12,7 +12,7 @@ function Road(name, time){
 }
 
 var cities = [{name: "A", firestation: false}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}, {name: "F", firestation: false}, {name: "G", firestation: true}];
-var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}, {cities: ["F", "A"], time: 53},{cities:["F", "C"], time:5},{cities: ["C", "G"], time: 6},{cities: ["B", "D"], time: 7}];
+var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}, {cities: ["F", "A"], time: 53},{cities:["F", "C"], time:25},{cities: ["C", "G"], time: 3},{cities: ["B", "D"], time: 7}];
 
 function nodefinder(name){
     var connections = roads.filter(elem => elem.cities.indexOf(name)!==-1);
@@ -35,65 +35,83 @@ function getCityByName(name){
     return result[0]
 }
 
+function isTooFar(element, index, array) {
+    return (element > 10);
+  }
 
-function hasFirestation(obj){
-    if (obj.firestation === true){
-        return true
-    }
-}
-// console.log(dijkstra(cityMap[0]))
-// console.log(nodefinder("A"))
-// console.log(getCityByName("B"));
-// breadthFirstSearch(cityMap[5]);
+
+
 var visitedTime = [];
 var visited = [];
 var unvisited=[];
+var dontGo=[]
 cityMap.forEach(el=>{
         unvisited.push(el.name)
 })
 function recursive (name){
     var object = getCityByName(name);
+    console.log("im in: " + object.name)
     unvisited.splice(unvisited.indexOf(name), 1) 
-    console.log(`unvisiteeeeeed ${unvisited}`)
-
-    if (hasFirestation(object)||unvisited.length===0){
-        console.log("found!!!!!!!!!!!!!!!!!");
-    return 
-    }else{
-        object.nodes.forEach(el=>{visitedTime.push(el.time)})
-        object.nodes.forEach(el=>{
-            if(el.cities[0]!==object.name){
-                visited.push(el.cities[0])
-            }else{visited.push(el.cities[1])
-            }})
-        console.log("visited time __________")
-        console.log(visitedTime);
-        console.log("visited time __________")
-        console.log(visited)
-        var citiesInRange = [];
-        var range = visitedTime.filter((el, index)=>{
-             if(el<=10){
-                 citiesInRange.push(visited[index])
-                 visitedTime[index]=visitedTime[index] + el
-             }return el<=10
-            })
-
-        console.log(citiesInRange);
-        console.log(range)
-        console.log("dive deeper");
-        var deeperNodes = [];
-        citiesInRange.forEach(el=>deeperNodes.push(getCityByName(el)))
-        console.log(deeperNodes)
-         if(range.length>0){
-            for (let i=0; i<deeperNodes.length; i++){
-            recursive(deeperNodes[i].name);
-            return 
-        }}
+    console.log(`unvisited ${unvisited}`)
+    console.log(object.nodes);
+    var deeperNodes = [];
+    for(let i=0; i<object.nodes.length;i++){
+        var otherCity = "";
+        if(object.nodes[i].cities[0]===object.name){
+            otherCity=object.nodes[i].cities[1];
+        }else{
+            otherCity=object.nodes[i].cities[0]
+        }
+        deeperNodes.push(otherCity)
+        if(object.nodes[i].time>10){
+            dontGo.push(otherCity)
+        }
     }
+    console.log(deeperNodes)
+    console.log('dontGo '+ dontGo)
 
-    // deeperNodes.forEach(el=>recursive(el.name))
-    // var deeperNodes = object.nodes.map(el=> recurring(el.cities[1]))
-    // console.log(deeperNodes)
+  
+
+
+
+    // if(object.nodes.some(el=>{
+    //     el.cities[0].firestation===true || el.cities[1].firestation===true
+    // })){
+    //     console.log("boo");
+    //     return
+    // }else{
+    //     if (unvisited.length===0){
+    //         console.log("found")
+    //     return 
+    //     }else{
+    //         object.nodes.forEach(el=>{visitedTime.push(el.time)})
+    //         object.nodes.forEach(el=>{
+    //             if(el.cities[0]!==object.name){
+    //                 visited.push(el.cities[0])
+    //             }else{visited.push(el.cities[1])
+    //             }})
+    //         var citiesInRange = [];
+    //         var range = visitedTime.filter((el, index)=>{
+    //              if(el<=10){
+    //                  citiesInRange.push(visited[index])
+    //                  visitedTime[index]=visitedTime[index] + el
+    //              }return el<=10
+    //             })
+    //         console.log("dive deeper");
+    //         console.log(citiesInRange)
+    //         var deeperNodes = [];
+    //         citiesInRange.forEach(el=>deeperNodes.push(getCityByName(el)))
+    //         console.log(deeperNodes)
+    //         console.log("big things happen now")
+    //          if(range.length>0){
+    //             for (let i=0; i<deeperNodes.length; i++){
+    //             recursive(deeperNodes[i].name);
+    //             return 
+    //         }}
+    //     }
+    
+    // }
+    
 
 }
 
