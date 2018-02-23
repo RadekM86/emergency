d92d5d5befb2157358883361be6b737ce3d678db
RadekM86 <byrm.design@gmail.com>
2018-02-23 20:43:13 +0100

    it finds the way, but what about time

--- public/algorithm.js
+++ public/algorithm.js
@@ -11,8 +11,8 @@ function Road(name, time){
     this.time=time
 }
 
-var cities = [{name: "A", firestation: false}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}, {name: "F", firestation: false}];
-var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}, {cities: ["F", "A"], time: 53}];
+var cities = [{name: "A", firestation: false}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}, {name: "F", firestation: false}, {name: "G", firestation: true}];
+var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}, {cities: ["F", "A"], time: 53},{cities:["F", "C"], time:5},{cities: ["C", "G"], time: 6}];
 
 function nodefinder(name){
     var connections = roads.filter(elem => elem.cities.indexOf(name)!==-1);
@@ -29,67 +29,12 @@ var firestationsMap = cityMap.filter(elem=>{
         return elem.firestation===true
 })
 
-console.log(cityMap)
-
-function pathToFirestation(object){
-    if (object.firestation === true){
-        console.log("found")
-        return
-    }else{
-        var time = object.nodes.map(el=>el.time)
-        var shortestTime = time.sort(function(a, b){return a-b})
-        console.log(shortestTime)
-        if(shortestTime[0]>10){ console.log("not found") }
-        else{console.log("found")}
-    }
-}
 
 function getCityByName(name){
     var result = cityMap.filter(el=>el.name===name)
     return result[0]
 }
 
-function breadthFirstSearch(object){
-    var time = object.nodes.map(el=>el.time)
-    var shortestTime = time.sort(function(a, b){return a-b})
-    if (object.firestation === true){
-        return "found"
-}
-    else if(shortestTime[0]<=10){
-        return "found"
-    }else{
-       console.log("not found")
-    }
-}
-
-function connectingNodes(name){
-    return getCityByName(name)
-}
-
-function dijkstra(object){
-
-    var visited=[];
-    var times=[];
-    var unvisited=cityMap;
-    var index = unvisited.indexOf(object);
-    if (index > -1) {
-        unvisited.splice(index, 1);
-    }
-    var closest = object.nodes
-    for(let i=0; i<closest.length; i++){
-        visited.push(object.nodes[i])
-        times.push(object.nodes[i].time)
-    }
-    console.log("visited")
-    console.log(visited)
-    console.log(times)
-    console.log("dive deeper");
-    closest.filter((el,index)=>{
-        el.cities.splice(el.cities.indexOf(object.name), 1)
-        el.cities.map(el=>getCityByName(el.cities))
-        dijkstra(el.cities)
-})
-}
 
 function hasFirestation(obj){
     if (obj.firestation === true){
@@ -150,4 +95,4 @@ function recursive (name){
 
 }
 
-recursive("A");
+recursive("F");
