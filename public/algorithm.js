d68c4e9861039baa158ca752f97108336b92107f
RadekM86 <byrm.design@gmail.com>
2018-02-23 16:48:05 +0100

    Working with algorithms

--- public/algorithm.js
+++ public/algorithm.js
@@ -11,11 +11,8 @@ function Connections(array, time){
     this.time = time
 }
 
-var cities = [{name: "A", firestation: true}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}];
-var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}];
-
-
-
+var cities = [{name: "A", firestation: false}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}, {name: "F", firestation: false}];
+var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}, {cities: ["F", "A"], time: 53}];
 
 function nodefinder(name){
    var connections = roads.filter(elem => elem.cities.indexOf(name)!==-1)
@@ -28,16 +25,41 @@ function cityBuilder(array){
 
 var cityMap = cityBuilder(cities);
 
-// var cityNodes = cityMap.map(elem=> elem.nodes=nodefinder(elem.name))
-
-// .map(el=>new City(name, el.firestation,el.time))
-
 var firestationsMap = cityMap.filter(elem=>{
         return elem.firestation===true
 })
 
-console.log(firestationsMap, cityMap);
+console.log(cityMap)
 
-// function dijkstra(Object, maximum){
+function pathToFirestation(object){
+    if (object.firestation === true){
+        console.log("Found!")
+        return
+    }else{
+        var time = object.nodes.map(el=>el.time)
+        var shortestTime = time.sort(function(a, b){return a-b})
+        console.log(shortestTime)
+        if(shortestTime[0]>10){ console.log( "ojoj") }
+        else{console.log("ok")}
+    }
+}
 
-// }
+// pathToFirestation(cityMap[5])
+
+function breadthFirstSearch(object){
+    var time = object.nodes.map(el=>el.time)
+    var shortestTime = time.sort(function(a, b){return a-b})
+    if (object.firestation === true){
+        return "found"
+}
+    else if(shortestTime[0]<=10){
+        return "found"
+    }else{
+        var closestCities = object.nodes.map(el=>el.cities.filter(el=>el!=object.name))
+        var getCities = closestCities.map(el=>nodefinder(el))
+        console.log(closestCities);
+        console.log(getCities)
+    }
+}
+
+breadthFirstSearch(cityMap[5]);
