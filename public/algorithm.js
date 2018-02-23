44970d101c9eba75514a6138369968f506d138d4
RadekM86 <byrm.design@gmail.com>
2018-02-23 15:58:43 +0100

    Working object constructor from two databases: cities and roads

--- public/algorithm.js
+++ public/algorithm.js
@@ -1,40 +1,42 @@
 // shortest path algorithm in weighted undirected graph
 
-function City(name, firestation, nodes){
+function City(name, firestation){
     this.name=name;
     this.firestation = firestation;
+    this.nodes = nodefinder(this.name)
+}
+
+function Connections(array, time){
+    this.array = array;
+    this.time = time
 }
 
 var cities = [{name: "A", firestation: true}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}];
 var roads = [{cities: ["A", "B"], time: 4}, {cities: ["B", "C"], time: 10}, {cities: ["A", "C"], time: 8}, {cities: ["D", "E"], time: 3}];
 
 
-function cityBuilder(){
-    var cityMap = cities.map(el=>new City(el.name, el.firestation))
-    return cityMap
-}
+
 
 function nodefinder(name){
    var connections = roads.filter(elem => elem.cities.indexOf(name)!==-1)
    return connections
 }
+function cityBuilder(array){
+    var cityMap = array.map(el=>new City(el.name, el.firestation, nodefinder(el.name)))
+    return cityMap
+}
 
-var cityMap = cityBuilder();
+var cityMap = cityBuilder(cities);
 
+// var cityNodes = cityMap.map(elem=> elem.nodes=nodefinder(elem.name))
 
 // .map(el=>new City(name, el.firestation,el.time))
 
-var cityA = new City("A", true, ["B", "C"])
-
-var firestationsMap = cities.filter(elem=>{
+var firestationsMap = cityMap.filter(elem=>{
         return elem.firestation===true
-    })
+})
 
-
-
-
-console.log(firestationsMap);
-console.log(cityA)
+console.log(firestationsMap, cityMap);
 
 // function dijkstra(Object, maximum){
 
