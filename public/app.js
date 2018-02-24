f939d7cd59cbd8d87418d40be494f9fb3ee4c5a9
RadekM86 <byrm.design@gmail.com>
2018-02-24 13:38:51 +0100

    implementing algorithm into api

--- public/app.js
+++ public/app.js
@@ -1,11 +1,111 @@
 $(function() {
     
-        // variables for DOM
+// variables for DOM
         var ul = $(".list");
     
         var url = "http://localhost:3000";
     
+//Algorithm functions
+
+// shortest path algorithm in weighted undirected graph
+
+function City(name, firestation, nodes){
+  this.name=name;
+  this.firestation = firestation;
+  this.nodes = nodes;
+}
+
+function Nodes(cities, time){
+  this.name = cities.join("");
+  this.time = time
+}
+
+var max = 10;
+// var cities = [{name: "A", firestation: false}, {name: "B", firestation: true}, {name: "C", firestation: false}, {name: "D", firestation: false}, {name: "E", firestation: false}, {name: "F", firestation: false}, {name: "G", firestation: true}, {name: "W", firestation: false}];
+// var roads = [{name: ["A", "B"], time: 4}, {name: ["B", "C"], time: 10}, {name: ["A", "C"], time: 8}, {name: ["D", "E"], time: 3}, {name: ["F", "A"], time: 5},{name:["F", "C"], time:5},{name: ["C", "G"], time: 1},{name: ["D", "B"], time: 7}, {name: ["A", "W"], time: 12}];
+
+var cities = [];
+var roads = [];
+
+function nodefinder(name){
+  let newArr =[];
+  arr = roads.filter(elem => elem.name.indexOf(name)!==-1);
+  arr.map(el=>newArr.push(new Nodes(el.name.filter(elem=>elem!==name), el.time)))
+  return newArr
+}
+
+function cityBuilder(array){
+  var cityMap = array.map(el=>new City(el.name, el.firestation, nodefinder(el.name)))
+  return cityMap
+}
+
+
+var cityMap = cityBuilder(cities);
+
+var firestations = cityMap.filter(el=>el.firestation===true);
+
+function getCityByName(name){
+  var result = cityMap.filter(el=>el.name===name)
+  return result[0]
+}
+
+
+var searchTimesArray = [];
+var fastestResponse = [];
+
+let reversedBFS = (name, goal, time) =>{
+  let object = getCityByName(name);
+  console.log("I'm in: " + object.name + " looking for " + goal);
+  console.log(object.nodes)
+  let nodesArray = object.nodes.map(el=>el.name);
+  let timesArray = object.nodes.map(el=>el.time);
+  let searchTime = timesArray[nodesArray.indexOf(goal)]+time;
+  searchTimesArray.push(searchTime);
+  fastestResponse = searchTimesArray.sort(function(a, b){return a-b})
+  console.log(searchTime)
+  console.log(nodesArray.indexOf(goal))
+  if(fastestResponse[0]<max){
+      if (nodesArray.indexOf(goal)>-1){
+          return true
+      }else{
+          object.nodes.forEach(el=> reversedBFS(el.name, goal, el.time))
+      }
+  }else{
+      return false
+}
+}
+
+
+let search = (goal) => {
+  searchTimesArray = [];
+  let firestationsNames = firestations.map(el=>el.name);
+  console.log(firestationsNames)
+  if (firestationsNames.indexOf(goal)!==-1){
+      searchTimesArray.push(0)
+      return true
+  }else{
+      for (let i = 0; i<firestations.length; i++){
+          if(reversedBFS(firestations[i].name, goal, 0)===true){
+              return true
+          }
+      }return false
+  }
+}
+
+let shortestWay = (name)=>{
+  do{
+      search(name)
+  }
+  while(fastestResponse[0]>max);
+  return search(name)
+}
+
+//  Ajax functions     
     function insertCity(cities){
+      var cityMap = cityBuilder(cities);
+      var firestations = cityMap.filter(el=>el.firestation===true);
+      console.log(cityMap);
+      console.log(roads)
             cities.forEach(function(cities){
               console.log(cities);
               var li = $('<li>')
@@ -14,13 +114,17 @@ $(function() {
               var deleteBtn = $("<button>").html('Usuń <i class="fa fa-remove"></i>').addClass('delete');
               deleteBtn.data('id', cities.id);
               cities.firestation==="true" && li.addClass('firestation')
-              // var editBtn = $("<button>").text("Edytuj").addClass('edit');
-              // editBtn.data('id', cities.id);
               li.append(title);
               li.append(newFireStation)
               li.append(deleteBtn);
+              if(shortestWay(cities.name)===true){
+                li.addClass('inRange')
+              }else{
+                li.addClass('outOfRange')
+              }
               // li.append(editBtn);
-              ul.append(li)
+              ul.append(li);
+
             });
     };
     
@@ -57,7 +161,12 @@ $(function() {
               li.append(newTitle);
               li.append(newFireStation)
               li.append(deleteBtn);
-              response.firestation==="true" && li.addClass('firestation')
+              response.firestation==="true" && li.addClass('firestation');
+              if(shortestWay(response.name)===true){
+                li.addClass('inRange')
+              }else{
+                li.addClass('outOfRange')
+              }
               $('.get_name').val('')
               // li.append(editBtn);
               ul.append(li);
@@ -95,6 +204,22 @@ $(function() {
       });
     
     }
+
+    function getMax(){
+      $.ajax({
+      method: "GET",
+      url: url + "/max",
+      dataType: "json"
+      })
+        .done(function(response){
+        max = response.time
+      
+        })
+        .fail(function(error){
+        console.log('Error');
+        console.log(error);
+      })
+      };
     
     function getcities(){
     $.ajax({
@@ -103,8 +228,9 @@ $(function() {
     dataType: "json"
     })
       .done(function(response){
+      getRoads();
       insertCity(response);
-    
+        
     
       })
       .fail(function(error){
@@ -112,13 +238,29 @@ $(function() {
       console.log(error);
     })
     };
-    
-  getcities();
-  removeCity();
-  addCity();
-  // editCity();
-    
-    
-    
-    });
+
+    function getRoads(){
+      $.ajax({
+      method: "GET",
+      url: url + "/roads",
+      dataType: "json"
+      })
+        .done(function(response){
+        roads = response.map(el=>el)
+      
+        })
+        .fail(function(error){
+        console.log('Error');
+        console.log(error);
+      })
+      };
+getMax();
+console.log(max)
+getRoads();
+getcities();
+removeCity();
+addCity();
+
+  // end of code
+});
     
